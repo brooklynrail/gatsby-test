6e05492cc08686c59b370a8a515997314c767566
Aidan Feldman <aidan.feldman@gmail.com>
2019-05-08 03:06:22 -0400

    remove HTML tags from article title list

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,29 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
+import striptags from "striptags"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+
+const renderArticle = article => {
+  const title = striptags(article.title)
+  return (
+    <li key={article.id}>
+      <Link
+        to={article.permalink}
+        dangerouslySetInnerHTML={{ __html: title }}
+      />
+    </li>
+  )
+}
 
 const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <ul>
-      {data.allMySqlResults.edges.map(({ node }) => (
-        <li key={node.id}>
-          <Link to={node.permalink}>{node.title}</Link>
-        </li>
-      ))}
+      {data.allMySqlResults.edges.map(({ node: article }) =>
+        renderArticle(article)
+      )}
     </ul>
   </Layout>
 )
