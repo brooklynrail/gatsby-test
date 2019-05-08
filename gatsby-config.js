aeec560429f28c32d8ad09d694bf238de49286e6
Aidan Feldman <aidan.feldman@gmail.com>
2019-05-08 02:24:34 -0400

    list the article titles on the homepage

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,20 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-source-mysql",
+      options: {
+        connectionDetails: {
+          host: "localhost",
+          user: "brooklynrail",
+          password: "devpass",
+          database: "brooklynrail",
+          port: 3307,
+        },
+        query: "SELECT * FROM articles WHERE permalink IS NOT NULL",
+        idFieldName: "id",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
@@ -15,7 +15,25 @@ const IndexPage = () => (
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
+    <ul>
+      {data.allMySqlResults.edges.map(({ node }) => (
+        <li key={node.id}>{node.title}</li>
+      ))}
+    </ul>
   </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    allMySqlResults(limit: 100) {
+      edges {
+        node {
+          id
+          title
+        }
+      }
+    }
+  }
+`
