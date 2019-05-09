dd60c20f306986cf3dbf915819e13202e0cefe09
Aidan Feldman <aidan.feldman@gmail.com>
2019-05-09 00:41:14 -0400

    fix module exporting
    Since the helper file is being used in Node without Webpack, can't use
    ES6 module syntax for exporting.
    
    https://github.com/brooklynrail/brooklynrail/issues/341#issuecomment-490723109

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,2 +1,2 @@
 // for some reason the `id`s always come back as `Something_Something_NNN` - we just want the last part
-export const graphqlLongIdToShort = long => parseInt(long.split(`__`)[2])
+exports.graphqlLongIdToShort = long => parseInt(long.split(`__`)[2])
